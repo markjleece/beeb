a94a85f30b135cd30f4bce0d3ec99e18e8c84a94
mark leece <markjleece@gmail.com>
2025-07-05 20:03:48 -0700

    Changes:  - Stop enemies from using their weapon when K9 is on an elevator  - Increased the size of switch pairings data block to 48 bytes.  - Consolidated god-mode code into thekeyboardInputHandlers routine  - Added debug mode to help test levels  - Defined missing size constants  - Updated documentation (for above)

--- LevelEditor/model/SwitchGrid.cs
+++ LevelEditor/model/SwitchGrid.cs
@@ -10,7 +10,7 @@ namespace LevelEditor
 {
     class SwitchGrid
     {
-        static readonly int SerializedSize = 32;
+        static readonly int SerializedSize = 48;
 
         internal SwitchGrid Clone()
         {
